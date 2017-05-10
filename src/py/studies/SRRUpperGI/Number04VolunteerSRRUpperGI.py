af4890da44a0c9dd69caa7368f682a5b5bbf4d2d
Michael Ebner <michael.ebner.14@ucl.ac.uk>
2017-05-10 23:43:15 +0100

    ENH: Added subject SRR Upper GI #4
    Former-commit-id: c532a7a7f5f9f1780bbb561974867d0b21ac5851

--- src/py/studies/SRRUpperGI/Number04VolunteerSRRUpperGI.py
+++ src/py/studies/SRRUpperGI/Number04VolunteerSRRUpperGI.py
@@ -4,13 +4,12 @@
 # \file Number03VolunteerSRRUpperGI.py
 # \brief      { item_description }
 #
-# SRR Upper GI Scan #3 (LP, 6 April 2017) 
-# Summary:  o First scan after having set abdominal protocol 
-#           o Same protocol was acquired at brain (but no 3D HR ref was done) 
-#           It still was a bit chaotic ...
+# SRR Upper GI Scan #4 (LH, 10 May 2017) 
+# Summary:  o Final acquisition protocol was used
+#           o First time, all sequences for both abdomen and brain 
 #
 #  \author Michael Ebner (michael.ebner.14@ucl.ac.uk)
-#  \date April 2017
+#  \date May 2017
 
 
 ## Import libraries
@@ -23,12 +22,12 @@ import os                       # used to execute terminal commands in python
 from studies.SRRUpperGI.VolunteerSRRUpperGI import VolunteerSRRUpperGI
 
 
-class Number03VolunteerSRRUpperGI(VolunteerSRRUpperGI):
+class Number04VolunteerSRRUpperGI(VolunteerSRRUpperGI):
 
     def __init__(self, select_anatomy="upper_gi"):
 
-        self._directory_upper_gi = "/Users/mebner/UCL/Data/Data_SRRUpperGIAnatomyStudy/SRR03/nifti/"
-        self._directory_brain = "/Users/mebner/UCL/Data/Data_SRRUpperGIAnatomyStudy/SRR03/nifti/brain/"
+        self._directory_upper_gi = "/Users/mebner/UCL/Data/Data_SRRUpperGIAnatomyStudy/SRR04/nifti/"
+        self._directory_brain = "/Users/mebner/UCL/Data/Data_SRRUpperGIAnatomyStudy/SRR04/nifti/brain/"
 
         VolunteerSRRUpperGI.__init__(self, select_anatomy=select_anatomy)
 
@@ -43,10 +42,9 @@ class Number03VolunteerSRRUpperGI(VolunteerSRRUpperGI):
         labels = []
 
         ## Standard anatomical planes (0.78 x 0.78 x 5)
-        filenames.append("T2WTSECorBHSENSEs1201a1012")   # 0.78 x 0.78 x 6 !!
-        # filenames.append("T2WTSEtraBHSENSEs1301a1013") # Double breath-hold!! -> exclude!?
-        filenames.append("T2WTSEtraBHSENSEs1501a1015")
-        filenames.append("T2WTSEsagBHSENSEs1401a1014")
+        filenames.append("T2WCorBHSENSEs801a1008")
+        filenames.append("T2WtraBHSENSEs901a1009")
+        filenames.append("T2WsagBHSENSEs1001a1010")
         
         labels.append(self._label_SST2W_standard + "_" + self._label_coronal)
         labels.append(self._label_SST2W_standard + "_" + self._label_transverse)
@@ -61,10 +59,10 @@ class Number03VolunteerSRRUpperGI(VolunteerSRRUpperGI):
         labels = []
         
         ## Oblique planes (0.78 x 0.78 x 5)
-        filenames.append("T2WTSECorBHSENSEs801a1008")
-        filenames.append("T2WTSECorBHSENSEs901a1009")
-        filenames.append("T2WTSECorBHSENSEs1001a1010")
-        filenames.append("T2WTSECorBHSENSEs1101a1011")
+        filenames.append("T2W354530BHSENSEs1101a1011")  # -35 +45 +30
+        filenames.append("T2W354530BHSENSEs1201a1012")  # +35 +45 -30
+        filenames.append("T2W354530BHSENSEs1301a1013")  # -35 -45 -30
+        filenames.append("T2W354530BHSENSEs1401a1014")  # +35 -45 +30
 
         for i in range(0, len(filenames)):
             labels.append(self._label_SST2W_oblique + "_" + str(i))
@@ -80,9 +78,9 @@ class Number03VolunteerSRRUpperGI(VolunteerSRRUpperGI):
         labels = []
 
         ## Offset standard anatomical planes (0.78 x 0.78 x 5)
-        filenames.append("T2WTSECorBHSENSEs1701a1017")   # 0.78 x 0.78 x 6 !!
-        filenames.append("T2WTSEtraBHSENSEs1901a1019")
-        filenames.append("T2WTSEsagBHSENSEs1801a1018")
+        filenames.append("T2WOFFCorBHSENSEs1501a1015")
+        filenames.append("T2WOFFtraBHSENSEs1601a1016")
+        filenames.append("T2WOFFsagBHSENSEs1701a1017")
 
         labels.append(self._label_SST2W_standard_offset + "_" + self._label_coronal)
         labels.append(self._label_SST2W_standard_offset + "_" + self._label_transverse)
@@ -94,11 +92,11 @@ class Number03VolunteerSRRUpperGI(VolunteerSRRUpperGI):
     ## Getter for BFFE filename
     def _get_filename_balanced_fast_field_echo_upper_gi(self):
         
-        ## Standard MRCP BTFE sequence: 0.9 x 0.9 x 6
+        ## Standard MRCP BTFE sequence (0.9 x 0.9 x 6)
         # filename = "BTFEBHSENSEs401a1004"
 
         ## 0.73 x 0.73 x 1.5
-        filename = "bFFECORSENSEs1601a1016"
+        filename = "BFFECORBHSENSEs701a1007"
 
         return filename, self._label_BFFE
 
@@ -106,20 +104,15 @@ class Number03VolunteerSRRUpperGI(VolunteerSRRUpperGI):
     ## Getter for heavily T2-weighted volumetric filename
     def _get_filename_heavily_t2_weighted_volume_upper_gi(self):
         
-        ## Standard MRCP 3D HR volume (heavily T2w vol)
-        filename = "sMRCP3DHRSENSEs701a1007" # 0.65 x 0.65 x 0.9
+        ## Standard MRCP 3D HR volume, i.e. heavily T2w vol (0.65 x 0.65 x 0.9)
+        filename = "sMRCP3DHRSENSEs601a1006"
 
         return filename, self._label_heavilyT2w
 
 
     ## Getter for T2-weighted volumetric filename
     def _get_filename_t2_weighted_volume_upper_gi(self):
-        
-        ## Bellow-gated 3D volume with SST2W contrast (blurry)
-        filename = "sMRCP3DHRSENSEs601a1006" # 0.65 x 0.65 x 0.9
-
-        return filename, self._label_T2w
-
+        pass
 
     ## ************************************************************************
     ## Brain     
@@ -130,9 +123,9 @@ class Number03VolunteerSRRUpperGI(VolunteerSRRUpperGI):
         labels = []
 
         ## Standard anatomical planes (0.78 x 0.78 x 5)
-        filenames.append("T2WTSECorBHSENSEs2701a1027")  # 0.78 x 0.78 x 6 !!
-        filenames.append("T2WTSEtraBHSENSEs2801a1028")
-        filenames.append("T2WTSEsagBHSENSEs3201a1032")
+        filenames.append("T2WCorBHSENSEs2801a1028")
+        filenames.append("T2WtraBHSENSEs2901a1029")
+        filenames.append("T2WsagBHSENSEs3001a1030")
         
         labels.append(self._label_SST2W_standard + "_" + self._label_coronal)
         labels.append(self._label_SST2W_standard + "_" + self._label_transverse)
@@ -147,11 +140,10 @@ class Number03VolunteerSRRUpperGI(VolunteerSRRUpperGI):
         labels = []
         
         ## Oblique planes (0.78 x 0.78 x 5)
-        filenames.append("T2WTSECorBHSENSEs2301a1023")
-        filenames.append("T2WTSECorBHSENSEs2401a1024")
-        filenames.append("T2WTSECorBHSENSEs2501a1025")
-        filenames.append("T2WTSECorBHSENSEs2601a1026")
-        # filenames.append("T2WTSECorBHSENSEs3601a1036") ## same direction as 1023!
+        filenames.append("T2W354530BHSENSEs3101a1031")  # -35 +45 +30
+        filenames.append("T2W354530BHSENSEs3201a1032")  # +35 +45 -30
+        filenames.append("T2W354530BHSENSEs3301a1033")  # -35 -45 -30
+        filenames.append("T2W354530BHSENSEs3401a1034")  # +35 -45 +30
 
         for i in range(0, len(filenames)):
             labels.append(self._label_SST2W_oblique + "_" + str(i))
@@ -165,9 +157,9 @@ class Number03VolunteerSRRUpperGI(VolunteerSRRUpperGI):
         labels = []
 
         ## Offset standard anatomical planes (0.78 x 0.78 x 5)
-        filenames.append("T2WTSECorBHSENSEs3001a1030")   # 0.78 x 0.78 x 6 !!
-        filenames.append("T2WTSEtraBHSENSEs3101a1031")
-        filenames.append("T2WTSEsagBHSENSEs3301a1033")
+        filenames.append("T2WOFFCorBHSENSEs3501a1035")
+        filenames.append("T2WOFFtraBHSENSEs3601a1036")
+        filenames.append("T2WOFFsagBHSENSEs3701a1037")
 
         labels.append(self._label_SST2W_standard_offset + "_" + self._label_coronal)
         labels.append(self._label_SST2W_standard_offset + "_" + self._label_transverse)
@@ -178,8 +170,12 @@ class Number03VolunteerSRRUpperGI(VolunteerSRRUpperGI):
 
     ## Getter for BFFE filename
     def _get_filename_balanced_fast_field_echo_brain(self):
+        
+        ## Standard MRCP BTFE sequence (0.9 x 0.9 x 6)
+        # filename = "BTFEBHSENSEs2401a1024"
+
         ## 0.73 x 0.73 x 1.5
-        filename = "bFFECORSENSEs2901a1029"
+        filename = "BFFECORBHSENSEs2701a1027"
 
         return filename, self._label_BFFE
 
@@ -187,14 +183,16 @@ class Number03VolunteerSRRUpperGI(VolunteerSRRUpperGI):
     ## Getter for heavily T2-weighted volumetric filename
     def _get_filename_heavily_t2_weighted_volume_brain(self):
         
-        ## Standard as in abdominal but worse FOV selection
-        # filename = "sMRCP3DHRSENSEs3401a1034" # 0.65 x 0.65 x 0.9
-
-        ## Rotated such that better coverage of brain
-        filename = "sMRCP3DHRSENSEs3501a1035" # 0.65 x 0.65 x 0.9
+        ## Standard MRCP (0.65 x 0.65 x 0.9)
+        filename = "sMRCP3DHRSENSEs2601a1026"
 
         return filename, self._label_heavilyT2w
 
     ## Getter for T2-weighted volumetric filename
     def _get_filename_t2_weighted_volume_brain(self):
-        pass
+        
+        ## 3D T2 Brain (0.98 x 0.98 x 0.5)
+        filename = "3DBrainVIEWT2SENSEs3801a1038"
+
+        return filename, self._label_T2w
+        
