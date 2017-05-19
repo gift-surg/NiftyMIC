36c860fa0e8c760ec3b4547714324dcf1caddf83
Michael Ebner <michael.ebner.14@ucl.ac.uk>
2017-05-19 21:18:49 +0100

    DOC: Code structure
    Former-commit-id: a5c9334f3128cbb3d9995f3e869ba928acf1abb7

--- src/py/definitions.py
+++ src/py/definitions.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 dir_root = os.path.abspath(os.environ['VOLUMETRIC_RECONSTRUCTION_DIR'])
-dir_test = os.path.join(dir_root, 'test-data/')
+dir_test = os.path.join(dir_root, 'src/test-data/')
 dir_build_cpp = os.path.join(dir_root, 'build' , 'cpp')
 
 info = {
