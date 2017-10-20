8302a108fc2cb3943edbb18245283139a3bc7ac9
Michael Ebner <michael.ebner.14@ucl.ac.uk>
2017-10-20 15:42:15 +0100

    WIP: added all test modules to run_tests
    Former-commit-id: 530f447ae1e279a775623383af425630db43813b

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -16,17 +16,17 @@ import os
 
 # Import modules for unit testing
 from brain_stripping_test import *
-# from cpp_itk_registration_test import *
-# from differential_operations_test import *
-# from intensity_correction_test import *
-# from intra_stack_registration_test import *
-# from linear_image_quality_transfer_test import *
-# from niftyreg_test import *
-# from parameter_normalization_test import *
-# from registration_test import *
-# from segmentation_propagation_test import *
-# from simulator_slice_acquisition_test import *
-# from stack_test import *
+from cpp_itk_registration_test import *
+from differential_operations_test import *
+from intensity_correction_test import *
+from intra_stack_registration_test import *
+from linear_image_quality_transfer_test import *
+from niftyreg_test import *
+from parameter_normalization_test import *
+from registration_test import *
+from segmentation_propagation_test import *
+from simulator_slice_acquisition_test import *
+from stack_test import *
 
 
 if __name__ == '__main__':
