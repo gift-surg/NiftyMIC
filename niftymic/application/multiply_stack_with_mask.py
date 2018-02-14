472e5872caf1f79262db5b35d23549ae6834080f
Michael Ebner <michael.ebner.14@ucl.ac.uk>
2018-02-14 14:26:11 +0000

    STYLE: Added option 'dir-output' to multiply_stack_with_mask script

--- niftymic/application/multiply_stack_with_mask.py
+++ niftymic/application/multiply_stack_with_mask.py
@@ -26,6 +26,7 @@ def main():
     input_parser.add_gestational_age(required=True)
     input_parser.add_dir_input_templates(default=DIR_TEMPLATES)
     input_parser.add_verbose(default=1)
+    input_parser.add_dir_output()
     input_parser.add_prefix_output(default="Masked_")
     args = input_parser.parse_args()
     input_parser.print_arguments(args)
@@ -41,7 +42,12 @@ def main():
                                    extract_slices=False)
     stack_masked = stack.get_stack_multiplied_with_mask()
     stack_masked.set_filename(args.prefix_output + stack.get_filename())
-    stack_masked.write(os.path.dirname(args.filename))
+
+    if args.dir_output is None:
+        dir_output = os.path.dirname(args.filename)
+    else:
+        dir_output = args.dir_output
+    stack_masked.write(dir_output)
 
     if args.verbose:
         sitkh.show_stacks([stack, stack_masked], segmentation=stack)
