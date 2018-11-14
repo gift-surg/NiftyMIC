4eb9101e8533ba60cf4f42e1b3f7b6ec4e1921c0
Michael Ebner <michael.ebner.14@ucl.ac.uk>
2018-11-14 16:02:55 +0000

    ENH: script to build image that shows estimated intra-stack motion

---
+++ niftymic/prototyping/export_motion_estimates.py
@@ -0,0 +1,142 @@
+##
+# \file export_motion_estimates.py
+# \brief      Script to export stack motion estimates
+#
+# \author     Michael Ebner (michael.ebner.14@ucl.ac.uk)
+# \date       Nov 2018
+#
+
+import os
+import numpy as np
+
+import pysitk.python_helper as ph
+import pysitk.simple_itk_helper as sitkh
+
+import niftymic.base.stack as st
+import niftymic.base.data_reader as dr
+import niftymic.base.data_writer as dw
+import niftymic.utilities.motion_updater as mu
+import niftymic.prototyping.stack_motion_image_builder as smib
+from niftymic.utilities.input_arparser import InputArgparser
+
+
+def main():
+
+    time_start = ph.start_timing()
+
+    # Set print options for numpy
+    np.set_printoptions(precision=3)
+
+    # Read input
+    input_parser = InputArgparser(
+        description="Export image that illustrates the estimated "
+        "intra-stack motion by showing the mean displacements per slice for "
+        "all individual slice voxels in millimetre.",
+    )
+
+    input_parser.add_filenames(required=True)
+    input_parser.add_filenames_masks()
+    input_parser.add_dir_input_mc(required=True)
+    input_parser.add_dir_output(required=True)
+    input_parser.add_suffix_mask(default="_mask")
+    input_parser.add_log_config(default=False)
+    input_parser.add_verbose(default=0)
+    input_parser.add_argument(
+        "--multi-component", "-multi-component",
+        action='store_true',
+        help="If given, the input image filename is interpreted as "
+        "multi-component."
+    )
+
+    args = input_parser.parse_args()
+    input_parser.print_arguments(args)
+
+    if args.log_config:
+        input_parser.log_config(os.path.abspath(__file__))
+    # --------------------------------Read Data--------------------------------
+    ph.print_title("Read Data")
+
+    if args.multi_component:
+        if len(args.filenames) > 1:
+            raise IOError(
+                "Only one multi-component input image can be processed")
+
+        if args.filenames_masks is None:
+            filename_mask = None
+        else:
+            filename_mask = args.filenames_masks[0]
+        ph.print_info("Read multi-component image ...")
+        data_reader = dr.MultiComponentImageReader(
+            path_to_image=args.filenames[0],
+            path_to_image_mask=filename_mask,
+        )
+
+    else:
+        data_reader = dr.MultipleImagesReader(
+            file_paths=args.filenames,
+            file_paths_masks=args.filenames_masks,
+            suffix_mask=args.suffix_mask,
+        )
+
+    data_reader.read_data()
+    stacks_orig = data_reader.get_data()
+    ph.print_info("%d input stacks read for further processing" %
+                  len(stacks_orig))
+
+    # ------------------------------Update Motion------------------------------
+    motion_updater = mu.MotionUpdater(
+        stacks=stacks_orig,
+        dir_motion_correction=args.dir_input_mc,
+    )
+    motion_updater.run()
+    stacks = motion_updater.get_data()
+    stacks_motion = [None] * len(stacks)
+    suffix_output = "_mean_disp"
+
+    # ---------------Build image(s) to visualize applied motion---------------
+    stack_motion_image_builder = smib.StackMotionImageBuilder()
+    for i, stack in enumerate(stacks):
+        stack_motion_image_builder.set_stack(stack)
+        stack_motion_image_builder.set_stack_ref(stacks_orig[i])
+
+        stacks_motion[i] = \
+            stack_motion_image_builder.get_mean_displacement_image()
+
+    # -------------------------------Write Data--------------------------------
+    paths_to_output = []
+    if args.multi_component:
+        path_to_output = os.path.join(
+            args.dir_output,
+            ph.append_to_filename(os.path.basename(args.filenames[0]),
+                                  suffix_output)
+        )
+        data_writer = dw.MultiComponentImageWriter(
+            stacks_motion, path_to_output)
+        data_writer.write_data()
+
+        # verbose
+        paths_to_output.append(path_to_output)
+
+    else:
+        for s in stacks_motion:
+            filename = "%s%s" % (s.get_filename(), suffix_output)
+            s.set_filename(filename)
+
+            # verbose
+            paths_to_output.append(
+                os.path.join(args.dir_output, "%s.nii.gz" % filename))
+
+        data_writer = dw.MultipleStacksWriter(
+            stacks_motion, args.dir_output)
+        data_writer.write_data()
+
+    if args.verbose:
+        ph.show_niftis(paths_to_output)
+
+    print("Computational Time: %s" % ph.stop_timing(time_start))
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
---
+++ niftymic/prototyping/stack_motion_image_builder.py
@@ -0,0 +1,146 @@
+##
+# \file stack_motion_image_builder.py
+# \brief      Class to build image that shows estimated intra-stack motion,
+#             i.e. motion for each individual slice
+#
+#
+# \author     Michael Ebner (michael.ebner.14@ucl.ac.uk)
+# \date       Nov 2018
+#
+
+import os
+import re
+import numpy as np
+import SimpleITK as sitk
+
+import pysitk.python_helper as ph
+import pysitk.simple_itk_helper as sitkh
+import simplereg.utilities
+
+import niftymic.base.stack as st
+import niftymic.base.exceptions as exceptions
+
+
+##
+# Front-end to create stack motion image objects step by step.
+# \date       2018-11-11 17:06:15+0000
+#
+class StackMotionImageBuilder(object):
+
+    def __init__(self, stack=None, stack_ref=None):
+        self._stack = stack
+        self._stack_ref = stack_ref
+
+    def set_stack(self, stack):
+        self._stack = stack
+
+    def set_stack_ref(self, stack_ref):
+        self._stack_ref = stack_ref
+
+    def _check_inputs(self):
+        if self._stack_ref is None:
+            raise ValueError("Reference stack needs to be provided")
+
+        if self._stack_ref is not None:
+            if self._stack.sitk.GetSize() != self._stack_ref.sitk.GetSize():
+                raise ValueError(
+                    "Provided stacks must have identical data array sizes")
+
+    def get_mean_displacement_image(self):
+        self._check_inputs()
+
+        shape = self._stack.sitk.GetSize()[::-1]
+        nda = np.ones(shape) * np.inf
+
+        slices_ref = self._stack_ref.get_slices()
+        slices_ref_dic = {s.get_slice_number(): s for s in slices_ref}
+
+        # Compute mean displacements for all (non-discarded) slices
+        for slice in self._stack.get_slices():
+
+            i = slice.get_slice_number()
+            nda[i, :, :] = \
+                simplereg.utilities.get_mean_displacement_between_images(
+                    slice.sitk, slices_ref_dic[i].sitk
+            )
+
+        return self._get_stack_from_nda(nda)
+
+    ##
+    # Gets the absolute motion image, i.e. l1-norm of rigid transform
+    # parameters
+    # \date       2018-11-14 13:09:47+0000
+    #
+    # \param      self  The object
+    #
+    # \return     The absolute motion image as stack object.
+    #
+    def get_absolute_motion_image(self):
+        shape = self._stack.sitk.GetSize()[::-1]
+        nda = np.ones(shape) * np.inf
+
+        slices = self._stack.get_slices()
+        for slice in slices:
+            k = slice.get_slice_number()
+
+            parameters_nda = self._get_rigid_parameters(slice)
+
+            # Convert rotation parameters to degrees
+            parameters_nda[0:3] *= 180. / np.pi
+
+            # Compute l1-norm of motion parameters
+            nda[k, :, :] = np.sum(np.abs(parameters_nda))
+
+        return self._get_stack_from_nda(nda)
+
+    ##
+    # Gets the motion parameter image, i.e. multi-component image with all
+    # rigid transform parameters
+    # \date       2018-11-14 13:08:01+0000
+    #
+    # \param      self  The object
+    #
+    # \return     The motion image as stack object.
+    #
+    def get_motion_parameter_image(self):
+
+        # Assume rigid 3D motion, i.e. 6 degrees of freedom
+        shape = self._stack.sitk.GetSize()[::-1] + (6,)
+        nda = np.ones(shape, np.float32) * np.inf
+
+        slices = self._stack.get_slices()
+        for slice in slices:
+            k = slice.get_slice_number()
+
+            parameters_nda = self._get_rigid_parameters(slice)
+
+            # Convert rotation parameters to degrees
+            parameters_nda[0:3] *= 180. / np.pi
+
+            nda[k, :, :] = parameters_nda
+
+        return self._get_stack_from_nda(nda)
+
+    @staticmethod
+    def _get_rigid_parameters(slice):
+        transform_sitk = slice.get_motion_correction_transform()
+        euler_sitk = simplereg.utilities.extract_rigid_from_affine(
+            transform_sitk)
+        parameters_nda = np.array(euler_sitk.GetParameters())
+
+        return parameters_nda
+
+    def _get_stack_from_nda(self, nda):
+        image_sitk = sitk.GetImageFromArray(nda)
+        image_sitk.SetOrigin(self._stack_ref.sitk.GetOrigin())
+        image_sitk.SetSpacing(self._stack_ref.sitk.GetSpacing())
+        image_sitk.SetDirection(self._stack_ref.sitk.GetDirection())
+
+        stack = st.Stack.from_sitk_image(
+            image_sitk=image_sitk,
+            filename=self._stack.get_filename(),
+            slice_thickness=self._stack_ref.get_slice_thickness(),
+            image_sitk_mask=self._stack_ref.sitk_mask,
+            extract_slices=False,
+        )
+        return stack
