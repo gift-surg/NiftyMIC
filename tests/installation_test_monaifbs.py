08b9abe1917f3b06f2266b0cdefceef99f71b2e5
Marta Ranzini <marta.ranzini.15@ucl.ac.uk>
2020-11-24 11:36:48 +0000

    separated tests for installation of fetal_brain_seg and monaifbs

--- tests/installation_test_monaifbs.py
+++ tests/installation_test_monaifbs.py
@@ -24,7 +24,7 @@ class InstallationTest(unittest.TestCase):
         self.path_to_image = os.path.join(DIR_TEMPLATES, "STA23.nii.gz")
 
     ##
-    # Test whether fetal_brain_seg can be called
+    # Test whether monaifbs can be called
     # \date       2019-03-24 14:14:18+0000
     #
     def test_fetal_brain_seg(self):
@@ -33,7 +33,6 @@ class InstallationTest(unittest.TestCase):
         cmd_args = ["niftymic_segment_fetal_brains"]
         cmd_args.append("--filenames %s" % self.path_to_image)
         cmd_args.append("--dir-output %s" % dir_output)
-        cmd_args.append("--neuroimage-legacy-seg 1")
         # cmd_args.append("--verbose 1")
         cmd = " ".join(cmd_args)
 
