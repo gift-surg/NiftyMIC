39cf66a2fbd3f3336a5cbca7ef60e2cc271ee597
Michael Ebner <michael.ebner@kcl.ac.uk>
2022-02-13 14:55:59 +0000

    DOC: Added demo images for rs-fMRI algorithm description

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -13,6 +13,7 @@ import unittest
 
 from brain_stripping_test import *
 from case_study_fetal_brain_test import *
+from case_study_rsfmri_test import *
 from data_reader_test import *
 from image_similarity_evaluator_test import *
 from intensity_correction_test import *
@@ -23,7 +24,6 @@ from segmentation_propagation_test import *
 # from simulator_slice_acquisition_test import *  # only in dev branch
 from stack_test import *
 
-# from case_study_rsfmri_test import *
 
 # from parameter_normalization_test import *
 # from registration_test import *  # TBC
